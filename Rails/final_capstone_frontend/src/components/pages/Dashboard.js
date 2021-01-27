f0b613de50a96d690c6544c461d033324e32870e
Leonardo Mezu <leo.mez@outlook.com>
2021-01-27 09:44:26 -0500

    Basic LogIn Logout structure Actions Reducers done

--- Rails/final_capstone_frontend/src/components/pages/Dashboard.js
+++ Rails/final_capstone_frontend/src/components/pages/Dashboard.js
@@ -1,5 +1,4 @@
 import { useSelector } from 'react-redux';
-import LogoutButton from '../LogoutButton';
 
 const Dashboard = () => {
   const loggedIn = useSelector(state => state.auth.loggedIn);
@@ -7,7 +6,6 @@ const Dashboard = () => {
   const main = (
     <>
       <h1>I `&aposm;` a dashboard</h1>
-      <LogoutButton />
       {`${loggedIn}`}
     </>
   );
